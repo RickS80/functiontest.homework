c1ce431d980807d4bba5d82f2aee0ce56d5729ae
Davide Venditti <davide_335@msn.com>
2018-11-20 17:02:06 +0000

    Added 3 more functions

--- start_point/ruby_functions_practice.rb
+++ start_point/ruby_functions_practice.rb
@@ -11,5 +11,17 @@ def subtract(num1, num2)
 end
 
 def multiply(num1, num2)
-  return num1 * num2 
+  return num1 * num2
+end
+
+def length_of_string(test_string)
+  test_string.length
+end
+
+def join_string(string1, string2)
+  return "#{string1}#{string2}"
+end
+
+def add_string_as_number (string1, string2)
+  string1.to_i + string2.to_i
 end
