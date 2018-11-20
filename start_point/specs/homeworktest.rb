c490320250740d7c4d6ec20e094709f8cb32131e
Rick Symington <rick.symington@gmail.com>
2018-11-20 20:36:20 +0000

    Integers to months using case & testing file

--- start_point/specs/homeworktest.rb
+++ start_point/specs/homeworktest.rb
@@ -1,35 +1,3 @@
-def return_10()
-  return 10
-end
-
-def add(num1,num2)
-  return num1 + num2
-end
-
-def subtract(num1, num2)
-  return num1 - num2
-end
-
-def multiply(num1, num2)
-  return num1 * num2
-end
-
-def divide(num1, num2)
-  return num1 / num2
-end
-
-def length_of_string(test_string)
-  p test_string.length
-end
-
-def join_string(string1, string2)
-  return "#{string1}#{string2}"
-end
-
-def add_string_as_number (string1, string2)
-  string1.to_i + string2.to_i
-end
-
 def number_to_full_month_name(num)
     case num
     when 1
@@ -58,3 +26,5 @@ def number_to_full_month_name(num)
       return "December"
     end
   end
+
+  p number_to_full_month_name(1)
